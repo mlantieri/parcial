7bbfa2e8310b5b3b4cd482c28ad212cc1ea2e34a
mlantieri <mlantieri@uade.edu.ar>
2025-11-14 20:22:19 -0300

    Segundo Commit

--- scripts.js
+++ scripts.js
@@ -2,6 +2,34 @@ const cambio = document.getElementById("tema");
 
 cambio.addEventListener("click", () => {
   const temaActual = document.documentElement.getAttribute("tema");
-  const temaNuevo = temaActual === "dark" ? "claro" : "oscuro";
-  document.documentElement.setAttribute("data-theme", temaNuevo);
+  const temaNuevo = temaActual === "oscuro" ? "claro" : "oscuro";
+  document.documentElement.setAttribute("tema", temaNuevo);
+});
+
+const form = document.getElementById("form");
+const error = document.getElementById("error");
+
+form.addEventListener("submit", () => {
+  let nombre = document.getElementById("nombre").value.trim();
+  let email = document.getElementById("email").value.trim();
+  let comentarios = document.getElementById("comentarios").value.trim();
+
+  error.textContent = "";
+
+  if (nombre === "") {
+    error.textContent = "Se requiere ingresar el nombre";
+    return;
+  }
+
+  if (email === "") {
+    error.textContent = "Se requiere ingresar el email";
+    return;
+  }
+
+  if (comentarios === "") {
+    error.textContent = "Se requiere ingresar comentarios";
+    return;
+  }
+  error.style.color = "green";
+  error.textContent = "Enviado correctamente";
 });
